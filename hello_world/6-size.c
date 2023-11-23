d24f00093bd87242a41d0333ed4dc42bb6325249
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-11-23 18:39:57 +0200

    Update 6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -12,7 +12,7 @@ long int c;
 long long int d;
 float e;
 printf("Size of a char: %d byte(s)\n", sizeof(a));
-printf("Size of a int: %d byte(s)\n", sizeof(b));
+printf("Size of an int: %d byte(s)\n", sizeof(b));
 printf("Size of a long int: %d byte(s)\n", sizeof(c));
 printf("Size of a long long int: %d byte(s)\n", sizeof(d));
 printf("Size of a float: %d byte(s)\n", sizeof(e));
