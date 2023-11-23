05056ad3156261a4dac426ef87dd3c4709970a8f
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-11-23 18:23:33 +0200

    Update 4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
-  * main - Prints a text
-  *
-  * Return: Always 0 (Success)
-  */
+* main - Prints a text
+*
+* Return: Always 0 (Success)
+*/
 int main(void)
 {
-    puts("\"Programming is like building a multilingual puzzle");
-    return (0);
+ puts("\"Programming is like building a multilingual puzzle");
+ return (0);
 }
