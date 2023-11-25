4482683556dae509331086a5d439702fefcb062d
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-11-25 17:50:55 +0200

    Update 0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -2,22 +2,20 @@
  
 int main () {
 
-   /* local variable definition */
-   int n = 100;
- 
-   /* check the boolean condition */
-   if( n > 0 ) {
-      /* if condition is true then print the following */
-      printf("n is positive\n" );
-   } else if( n == 0 ) {
-      /* if condition is false then print the following */
-      printf("n is 0\n" );
-   } else {
-      /* if condition is false then print the following */
-      printf("n is negative\n" );
-   }
-   
-   printf("value of n is : %d\n", n);
- 
-   return 0;
+/* local variable definition */
+int n = 100;
+  
+printf("value of n is : %d\n", n);
+/* check the boolean condition */
+if( n > 0 ) {
+/* if condition is true then print the following */
+printf("n is positive\n" );
+} else if( n == 0 ) {
+/* if condition is false then print the following */
+printf("n is 0\n" );
+} else {
+/* if condition is false then print the following */
+printf("n is negative\n" );
+}
+return 0;
 }
