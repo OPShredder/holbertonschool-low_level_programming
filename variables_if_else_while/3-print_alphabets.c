0aa60dde7a5f9ab8265c598d3f5c1709b686651b
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-11-28 18:06:51 +0200

    Update 3-print_alphabets.c

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 /**
-* main - prints the alphabet in lowercase and then in uppercase, 
+* main - prints the alphabet in lowercase and then in uppercase,
 * followed by a new line
 * Return: Always 0 (Success)
 */
