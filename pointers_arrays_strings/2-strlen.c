d7a4a3924baca9b1527e7c8753f674782a9ac7a0
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-08 17:34:56 +0200

    Update 2-strlen.c

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- *_strlen - returns the length of a string
- * @s: string
- *Return: returns lenght;
- */
+*_strlen - returns the length of a string
+* @s: string
+*Return: returns lenght;
+*/
 int _strlen(char *s)
 {
 int count, inc;
