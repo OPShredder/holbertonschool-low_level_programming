1c29a284aa0468c45b46d2745c58ac735e8ae861
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-09 22:44:53 +0200

    Create main.h

---
+++ static_libraries/main.h
@@ -0,0 +1,19 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <unistd.h>  /* Include any necessary libraries */
+
+/* Function prototypes */
+int _putchar(char c);
+int _isdigit(int c);
+int _islower(int c);
+int _isupper(int c);
+int _strlen(char *s);
+void _puts(char *s);
+char *_strcpy(char *dest, const char *src);
+int _isalpha(int c);
+
+
+/* Add other function prototypes here */
+
+#endif /* MAIN_H *
