fc74b24cf0d699271a6fa0c59dc499450f6133c3
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-09 16:28:30 +0200

    Update 0-puts_recursion.c

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,1 +1,26 @@
+#include "main.h"
 
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ *
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+/**
+ * _puts_recursion - Prints a string, followed by a new line.
+ * @s: The string to be printed.
+ */
+void _puts_recursion(char *s)
+{
+if (*s == '\0')
+{
+_putchar('\n'); /* Print new line when end of string is reached */
+return;
+}
+
+_putchar(*s); /* Print current character */
+_puts_recursion(s + 1); /* Recursively call function with the next character */
+}
