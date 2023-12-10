4e210a827291dd7f140873f60ccded1cf97eff6a
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-10 12:40:31 +0200

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -1,12 +1,12 @@
 #include "dog.h"
 
 /**
- * init_dog - Initializes a struct dog.
- * @d: Pointer to the struct dog to initialize.
- * @name: Name of the dog.
- * @age: Age of the dog.
- * @owner: Owner of the dog.
- */
+* init_dog - Initializes a struct dog.
+* @d: Pointer to the struct dog to initialize.
+* @name: Name of the dog.
+* @age: Age of the dog.
+* @owner: Owner of the dog.
+*/
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 if (d != (struct dog *)0)
