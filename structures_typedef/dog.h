3d02afe7ec1e34e93499e31fdb3bd624d5d4cf75
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-10 12:54:07 +0200

    Update dog.h

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -2,15 +2,15 @@
 #define DOG_H
 
 /**
- * struct dog - Structure representing a dog
- * @name: Name of the dog
- * @age: Age of the dog
- * @owner: Owner of the dog
- */
-typedef struct dog {
-    char *name;
-    float age;
-    char *owner;
+* struct dog - Structure representing a dog
+* @name: Name of the dog
+* @age: Age of the dog
+* @owner: Owner of the dog
+*/
+typedef struct dog{
+char *name;
+float age;
+char *owner;
 } dog_t;
 
 /* Function prototypes */
