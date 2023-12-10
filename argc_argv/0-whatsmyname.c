76f13a597448b38177eee974c299ad95dee2141a
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-10 10:28:37 +0200

    Create 0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point, prints the name of the program
+ * @argc: The number of arguments passed to the program
+ * @argv: Array of pointers to the arguments
+ * Return: Always 0
+ */
+
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+printf("%s\n", argv[0]);
+return (0);
+}
