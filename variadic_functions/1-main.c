df9e99193e7a9464cb074648685af3f4ebc2c6d4
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-10 21:21:28 +0200

    Create 1-main.c

---
+++ variadic_functions/1-main.c
@@ -0,0 +1,12 @@
+#include "variadic_functions.h"
+
+/**
+* main - check the code .
+*
+* Return: Always 0.
+*/
+int main(void)
+{
+print_numbers("; ", 5, 30, 938, -123024, 43402, 0);
+return (0);
+}
