b5c5bb7614d5dbf9d19cb8ef359122199ffe5a90
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-10 12:42:01 +0200

    Update 1-init_dog.c

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -7,8 +7,7 @@
 * @age: Age of the dog.
 * @owner: Owner of the dog.
 */
-void init_dog(struct dog *d, char *name, float age, char *owner)
-{
+void init_dog(struct dog *d, char *name, float age, char *owner){
 if (d != (struct dog *)0)
 {
 d->name = name;
