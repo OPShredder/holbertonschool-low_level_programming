d9ba27892eb1c498770f673059886c159d7fd377
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-10 20:37:55 +0200

    Create 1-print_numbers.c

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,29 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - returns the sum of all its parameters
+ * @n: the number of parameters
+ * @...: variable number of parameters to sum
+ *
+ * Return: sum of all parameters
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+va_list args;
+unsigned int i;
+int sum = 0;
+
+if (n == 0)
+return (0);
+
+va_start(args, n);
+
+for (i = 0; i < n; i++)
+{
+sum += va_arg(args, int);
+}
+
+va_end(args);
+
+return (sum);
+}
