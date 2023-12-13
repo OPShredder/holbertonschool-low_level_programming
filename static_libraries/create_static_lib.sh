4def8125ca33ac6f5c5ca438ab161218e8a00015
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-13 07:55:41 +0200

    Update create_static_lib.sh

--- static_libraries/create_static_lib.sh
+++ static_libraries/create_static_lib.sh
@@ -1,10 +1,3 @@
 #!/bin/bash
-
-# Compile all .c files into object files
-gcc -Wall -Werror -Wextra -pedantic -c *.c
-
-# Create the static library from the generated object files
-ar rcs liball.a *.o
-
-# Optional: Clean up - remove the object files after creating the library
-rm *.o
+gcc -c ./*.c
+ar -rc liball.a ./*.o
