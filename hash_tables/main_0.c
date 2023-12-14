ada25ac2d33ef0f6167a376dd4f87e85d0c9f5b8
marc habib <54110010+OPShredder@users.noreply.github.com>
2023-12-14 22:44:14 +0200

    Create main_0.c

---
+++ hash_tables/main_0.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include "hash_tables.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+int main(void)
+{
+	hash_table_t *ht;
+	size_t i;
+
+	ht = hash_table_create(2048);
+	if (!ht || ht->size != 2048 || !ht->array)
+		return (EXIT_FAILURE);
+	/*Loop through the array, should not SEGV*/
+	for (i = 0; i < 2048 && ht->array[i]; ++i);
+	return (EXIT_SUCCESS);
+}
